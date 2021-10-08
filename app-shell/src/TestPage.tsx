665fe3a9b956648f8a676938b96210a7de2338f6
sakayaparp <sakayaparp.p@thoughtworks.com>
2021-10-08 15:11:59 +0700

    [Game, Yod] Global state management

---
+++ app-shell/src/TestPage.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { useGlobalState } from 'piral';
+
+export const TestPage: React.FC = () => {
+  const test = useGlobalState((m) => m.language.selected);
+  return (
+    <>
+      <h3>TEST: {test}</h3>
+    </>
+  );
+};
--- app-shell/src/index.tsx
+++ app-shell/src/index.tsx
@@ -3,12 +3,13 @@ import { render } from 'react-dom';
 import { Redirect } from 'react-router-dom';
 import { createPiral, Piral, SetRoute } from 'piral';
 import { createContainersApi } from 'piral-containers';
+import { TestPage } from './TestPage';
 
 const piral = createPiral({
   requestPilets() {
     return fetch('http://localhost:9000/api/v1/pilet')
-      .then(res => res.json())
-      .then(res => res.items);
+      .then((res) => res.json())
+      .then((res) => res.items);
   },
   extendApi: [createContainersApi()],
 });
@@ -16,6 +17,7 @@ const piral = createPiral({
 const app = (
   <Piral instance={piral}>
     <SetRoute path="/" component={() => <Redirect from="/" to="/products" />} />
+    <TestPage />
   </Piral>
 );
 
